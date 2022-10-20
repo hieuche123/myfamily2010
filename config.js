2373cddd15a17810e0c3f4b1796faa8d0dc743d5
hieuche123 <86963416+hieuche123@users.noreply.github.com>
2022-10-20 08:04:06 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,13 @@
+const nameGirl = '';
+const giftUrl = 'https://www.facebook.com/messages/t/100022466296154';
+const eventName = 'Chúc bà, mẹ và 3 chị   20-10 vv';
+const titleCard = 'Gửi My Family';
+const contentCard = 'Chúc 4 cô công chúa của nhà ta có 1 ngày 20-10 thật ý nghĩa, hạnh phúc,tràn ngập niềm vui và những nụ cười không chỉ mỗi hôm nay mà ngày nào cũng vậy. Mong điều đẹp nhất sẽ đến với gia đình ta trong hôm nay và cả những ngày sau';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = 'z3813767281240_ad0ae2a9c7c2cd33cb0c478a8877e0ee.JPG';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
